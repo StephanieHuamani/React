8524b00ca6ab7f5baa20ede8f051d0cc85c449de
StephanieHuamani <lsvh97@hotmail.com>
2022-09-28 13:11:15 -0300

    Arreglo del itemDetail

--- src/containers/ItemDetailContainer.js
+++ src/containers/ItemDetailContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import ItemDetail from '../componentes/ItemDetail';
 import Loader from '../componentes/Loader';
 import { useParams } from "react-router-dom";
-import { doc, getDoc, query, where } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { db } from '../componentes/utils/firebaseConfig';
 
 const ItemDetailContainer = () => {
@@ -12,22 +12,12 @@ const ItemDetailContainer = () => {
 
     useEffect(() =>{
       setLoading(true);
-      const firesoreFetch = async () => {
-        let q
-        const docSnap = await getDoc(q);
-        const idFromFirestore = docSnap.doc.filter(document=>({
-          id: document.id,
-          ...document.data()
-        }))
-        if (docSnap.exists()) {
-          q = query(doc(db, "products"), where("id", "==", idDetail))
-        } else{
-          q = query(doc(db, "products"))
-        }
-        return idFromFirestore
+      const firesoreFetchId = async () => {
+        const docSnap = await getDoc(doc(db, "products", idDetail));
+        return {idDetail: idDetail, ...docSnap.data()}
     }
-    firesoreFetch()
-    .then(response => setData(response))
+    firesoreFetchId()
+    .then(result => setData(result))
     .finally(() => setLoading(false))
       }, [idDetail]);
     
