02316140ffc416e8da4ea79a08f1c5f77fe38ab4
StephanieHuamani <lsvh97@hotmail.com>
2022-10-05 14:17:26 -0300

    Item Collection II

--- src/componentes/Cart.js
+++ src/componentes/Cart.js
@@ -7,8 +7,13 @@ import { db } from "./utils/firebaseConfig"
 
 const Cart = () => {
   const { cartList, cartClear, totalItemPrice, totalItemPriceWithShipping} = useContext(CartContext);
-  const createOrder = async (e) => {
-    e.preventDefault()
+  const createOrder = async () => {
+    const itemsCart = cartList.map(item => ({
+      id: item.id,
+      title: item.title,
+      quantity: item.quantity,
+      price: item.price
+    }))
     let order = {
       buyer:{
         name: "Stephanie Huamani",
@@ -16,21 +21,22 @@ const Cart = () => {
         email: "stefHuama@hotmail.com"
       },
       date: serverTimestamp(),
-      items: cartList.map(item => ({id: item.id, title: item.title, quantity: item.quantity, price: item.price})),
+      items: itemsCart,
       total: totalItemPrice()
     }
 
-    const newOrderId = doc(collection(db, "order"))
+    const newOrderId = doc(collection(db, "orders"))
     await setDoc(newOrderId, order);
-    console.log(setDoc)
 
-    cartList.forEach( async item => {
-      const newListProducts = doc(db, "products", item.id);
-      await updateDoc(newListProducts, {
+    cartList.forEach( async (item) => {
+      const updateStock = doc(db, "products", item.id);
+      await updateDoc(updateStock, {
         stock: increment(-item.quantity)
       });
-    });
-    cartClear();
+    })
+    cartClear()
+
+    alert('Pedido realizado. Tu número de orden es:' + newOrderId.id)
   }
 
   return (
@@ -51,7 +57,7 @@ const Cart = () => {
             <p className='priceCart'>Subtotal (sin envío): ${totalItemPrice()}</p>
             <p className='priceCart'>Envio (precio único para todo el país): $300</p>
             <p className='finalPrice'>Total a pagar: ${totalItemPriceWithShipping()}</p>
-            <button className='btnFinalizarCompra' onClick={createOrder}>Finalizar Compra</button>
+            <button className='btnFinalizarCompra' onClick={() => createOrder()}>Finalizar Compra</button>
           </div>
         </div>
       }
